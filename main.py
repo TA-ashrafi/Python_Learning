eb49168b357669c1c276cf21c660d63f5e85e0ae
Tahseen Ashrafi <theperfectgamer100@gmail.com>
2025-08-19 12:03:19 +0530

    dvdjbgvdj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 a = 10
-b = 15 
+b = 15 dvndkfjvbdfkjv
 if a> b:
     print("a is greater than b")
 else:
-    print("b is greater than or equal to a")sdcbsjdhfbkdsfjsdbh
+    print("b is greater than or equal to a"
